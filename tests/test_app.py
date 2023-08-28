72404c2a634c26470fae904afe35461c0d68427d
naterocks49 <nshelhart@gmail.com>
2023-08-28 19:49:05 -0400

    Updated pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,21 @@
 import pytest
-from app import app
+from app import app as flask_app
 
 @pytest.fixture
 def app():
-    app = app()
-    return app
+    return flask_app
 
-'''def test_index_authenticated():
-    client = app.test_client()
-    with client.session_transaction() as session:
-        session['token_info'] = {'user_id': 123}
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Mac miller" in response.data
-    assert b"J Cole" in response.data
-    assert b"Kanye West" in response.data
+def test_index_authenticated(app):
+    with app.test_client() as client:
+        with client.session_transaction() as session:
+            session['token_info'] = {'user_id': 123}
+        response = client.get('/')
+        assert response.status_code == 200
+        assert b"Mac miller" in response.data
+        assert b"J Cole" in response.data
+        assert b"Kanye West" in response.data
 
-def test_index_unauthenticated():
+'''def test_index_unauthenticated():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 302
